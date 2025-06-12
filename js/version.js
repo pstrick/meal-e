7daf269aa8195026595c5939bac26e5b3c598b4e
Patrick Strickler <patrickstrickler@mac.lan>
2025-06-11 22:44:14 -0400

    feat: add Custom Ingredients link to all navigation menus

--- js/version.js
+++ js/version.js
@@ -1,7 +1,7 @@
 export const version = {
     year: 2025,
     month: 6,  // June
-    build: 60,  // Increment this with each push
+    build: 61,  // Increment this with each push
     toString: function() {
         return `${this.year}.${this.month}.${this.build}`;
     }
