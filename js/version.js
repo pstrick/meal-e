dfede97eef5d7c8cee647d93ceab2f5abd664374
Patrick Strickler <patrickstrickler@mac.lan>
2025-06-17 16:24:35 -0400

    Style: Make Add Meal button match standard button size in meal planner grid

--- js/version.js
+++ js/version.js
@@ -1,7 +1,7 @@
 export const version = {
     year: 2025,
     month: 6,  // June
-    build: 114,  // Increment this with each push
+    build: 115,  // Increment this with each push
     toString: function() {
         return `${this.year}.${this.month}.${this.build}`;
     }
