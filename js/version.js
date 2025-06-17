2fc8263a63b251e098e0e543aa97f0541c24c6c5
Patrick Strickler <patrickstrickler@mac.lan>
2025-06-17 14:14:16 -0400

    chore: remove version display from all pages

--- js/version.js
+++ js/version.js
@@ -1,7 +1,7 @@
 export const version = {
     year: 2025,
     month: 6,  // June
-    build: 86,  // Increment this with each push
+    build: 87,  // Increment this with each push
     toString: function() {
         return `${this.year}.${this.month}.${this.build}`;
     }
