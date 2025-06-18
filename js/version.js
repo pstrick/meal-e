8c5cbc616f38ac9d2059380062f1f23aff59e3ca
Patrick Strickler <patrickstrickler@mac.lan>
2025-06-17 22:10:48 -0400

    fix: remove hardcoded footers to prevent duplicate universal footer

--- js/version.js
+++ js/version.js
@@ -1,7 +1,7 @@
 export const version = {
     year: 2025,
     month: 6,  // June
-    build: 126,  // Increment this with each push
+    build: 127,  // Increment this with each push
     toString: function() {
         return `${this.year}.${this.month}.${this.build}`;
     }
