a3883d74869a3289b6257fb0ebceae20ebec7051
Patrick Strickler <patrickstrickler@mac.lan>
2025-06-17 22:51:41 -0400

    Fix ingredient-macros layout to display vertically

--- js/version.js
+++ js/version.js
@@ -1,7 +1,7 @@
 export const version = {
     year: 2025,
     month: 6,  // June
-    build: 138,  // Increment this with each push
+    build: 139,  // Increment this with each push
     toString: function() {
         return `${this.year}.${this.month}.${this.build}`;
     }
