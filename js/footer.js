7b745ad291372d744054c6fbe04b068d0cf56b45
Patrick Strickler <patrickstrickler@mac.lan>
2025-06-17 22:13:58 -0400

    fix: dynamically update version in universal footer to match version.js on all pages

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,10 @@
+import { version } from './version.js';
 // Inject universal footer
 fetch('footer.html')
   .then(res => res.text())
   .then(html => {
     document.body.insertAdjacentHTML('beforeend', html);
+    // Update version in the injected footer
+    const versionSpan = document.querySelector('footer .version');
+    if (versionSpan) versionSpan.textContent = `v${version.toString()}`;
   }); 
