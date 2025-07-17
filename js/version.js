c51541d8aad934aebc42e27c9bca1366c38bf3cb
Patrick Strickler <patrickstrickler@mac.lan>
2025-07-17 14:59:45 -0400

    Update version month to July (7)

--- js/version.js
+++ js/version.js
@@ -1,8 +1,8 @@
 export const version = {
     year: 2025,
-    month: 6,  // June
-    build: 143,  // Increment this with each push
+    month: 7,  // July
+    build: 144,  // Increment this with each push
     toString: function() {
         return `${this.year}.${this.month}.${this.build}`;
     }
-}; // Updated Tue Jun 17 15:55:43 EDT 2025
+}; // Updated Tue Jul 17 15:55:43 EDT 2025
