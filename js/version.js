bbafcea33346c80e5b82017a4a72d45e18425a9a
Patrick Strickler <patrickstrickler@mac.lan>
2025-11-12 15:56:33 -0500

    Make shopping list editor full screen

--- js/version.js
+++ js/version.js
@@ -2,7 +2,7 @@
 export const version = {
     year: 2025,
     month: 11,
-    build: 1,
+    build: 2,
     toString: function() {
         return `${this.year}.${this.month}.${this.build}`;
     }
