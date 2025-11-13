3ceb9704b5ea4e8b4f977f51aff082c14b6a812f
Patrick Strickler <patrickstrickler@mac.lan>
2025-11-12 22:03:06 -0500

    Make shopping items modal full screen

--- js/version.js
+++ js/version.js
@@ -2,7 +2,7 @@
 export const version = {
     year: 2025,
     month: 11,
-    build: 27,
+    build: 28,
     toString: function() {
         return `${this.year}.${this.month}.${this.build}`;
     }
