6e4369c53380983cab580f461a9608a2842a00f9
Patrick Strickler <patrickstrickler@mac.lan>
2025-11-18 16:16:31 -0500

    Fix vertical alignment of search and category fields on recipes page

--- js/version.js
+++ js/version.js
@@ -2,7 +2,7 @@
 export const version = {
     year: 2025,
     month: 11,
-    build: 51,
+    build: 52,
     toString: function() {
         return `${this.year}.${this.month}.${this.build}`;
     }
