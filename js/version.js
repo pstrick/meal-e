32c8ea2877f91e98c27e982dcd8feebf01b6f3bf
Patrick Strickler <patrickstrickler@mac.lan>
2025-11-18 21:28:07 -0500

    Add debug logging to amount input event listener

--- js/version.js
+++ js/version.js
@@ -2,7 +2,7 @@
 export const version = {
     year: 2025,
     month: 11,
-    build: 77,
+    build: 79,
     toString: function() {
         return `${this.year}.${this.month}.${this.build}`;
     }
