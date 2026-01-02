4b6ec13b107802136f3c95e1aaba7f4462e9efa1
Patrick Strickler <patrickstrickler@mac.lan>
2026-01-02 16:04:07 -0500

    Fix vertical alignment of ingredient table columns

--- js/version.js
+++ js/version.js
@@ -2,7 +2,7 @@
 export const version = {
     year: 2026,
     month: 1,
-    build: 21,
+    build: 22,
     toString: function() {
         return `${this.year}.${String(this.month).padStart(2, '0')}.${this.build}`;
     }
