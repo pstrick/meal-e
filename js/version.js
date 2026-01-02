cc2bd17233c0c130f0632edcbe68ec10753412ea
Patrick Strickler <patrickstrickler@mac.lan>
2026-01-02 15:06:22 -0500

    Fix octal literal error in version.js

--- js/version.js
+++ js/version.js
@@ -2,7 +2,7 @@
 export const version = {
     year: 2026,
     month: 01,
-    build: 4,
+    build: 5,
     toString: function() {
         return `${this.year}.${String(this.month).padStart(2, '0')}.${this.build}`;
     }
