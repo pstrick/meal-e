fbba47bfd710ff1dc2623362508cb1dc2cacd8a3
Patrick Strickler <patrickstrickler@mac.lan>
2026-01-23 21:34:19 -0500

    Hide Nutrition Tracker from navigation (keep page and code)

--- js/version.js
+++ js/version.js
@@ -2,7 +2,7 @@
 export const version = {
     year: 2026,
     month: 1,
-    build: 24,
+    build: 25,
     toString: function() {
         return `${this.year}.${String(this.month).padStart(2, '0')}.${this.build}`;
     }
